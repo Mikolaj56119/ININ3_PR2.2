8c662fd7f2ea223cdf27d66c36dca457dc906ebb
Mikolaj <niklaus7@me.com>
2022-01-16 21:28:10 +0100

    zad 9

--- src/com/company/Animals/FarmAnimals.java
+++ src/com/company/Animals/FarmAnimals.java
@@ -1,5 +1,16 @@
 package com.company.Animals;
 
-public class FarmAnimals {
+import com.company.Animal;
+
+public class FarmAnimals extends Animal implements Edbile {
+
+    public FarmAnimals(String species) {
+        super(species);
+    }
+
+    @Override
+    public void beEaten() {
+        System.out.println("Będzie zjedzony!");
+    }
 
 }
