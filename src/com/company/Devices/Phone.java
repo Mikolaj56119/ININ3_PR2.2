6ae83866b4e600d45ec778cf923d668a6a9a39a5
Mikolaj <niklaus7@me.com>
2022-01-18 19:39:32 +0100

    zad 10

--- src/com/company/Devices/Phone.java
+++ src/com/company/Devices/Phone.java
@@ -1,45 +1,89 @@
-package com.company.Devices;
+    package com.company.Devices;
 
-import com.company.Human;
-import com.company.Saleable;
+    import com.company.Human;
+    import com.company.Saleable;
 
-public class Phone extends Device implements Saleable {
-    final Double screenSize;
-    String os;
+    import java.net.URL;
+    import java.util.List;
 
-    public Phone(String producer, String model, Double screenSize, Integer yearOfProduction) {
-        super(yearOfProduction, producer, model);
-        this.screenSize = screenSize;
-    }
+    public class Phone extends Device implements Saleable {
+        final Double screenSize;
+        String os;
+        final static String DEFAULT_SERVER_ADDRESS = "22.48.16.2254";
+        final static String DEFAULT_PROTOCOL = "HTTPS";
+        final static String DEFAULT_VERSION = "11.15.4";
+        final static String DEFAULT_APPLICATION = "INTELLIJ";
 
-    @Override
-    public void turnOn() {
-        System.out.println("Klikam guzik wlaczajacy telefon");
-    }
-
-    @Override
-    public void sale() {
-
-    }
-
-    @Override
-    public void getPrice() {
-
-    }
-
-    @Override
-    public void sale(Human seller, Human buyer, Double price) {
-        System.out.println("Próba sprzedania telefonu " + this.producer);
-        if(seller.mobile != this) {
-            System.out.println("Sprzedawca nie ma telegonu");
-        } else if (buyer.cash < price) {
-            System.out.println("Sorry, kupujący ma za mało pieniędzy");
-        } else {
-            seller.cash += price;
-            buyer.cash -= price;
-            seller.mobile = null;
-            buyer.mobile = this;
-            System.out.println("Telefon "+ this.producer + " został sprzedany za "+ price);
+        public Phone(String model,String producer,Integer yearOfProduction, Double screenSize) {
+            super(yearOfProduction, producer, model);
+            this.screenSize = screenSize;
         }
+
+        @Override
+        public void turnOn() {
+
+            System.out.println("Włączenie telefonu");
+        }
+
+
+        @Override
+        public void sale(Human seller, Human buyer, Double price) {
+            System.out.println("Telefon na sprzedarz " + this.producer);
+            if(seller.mobile != this) {
+                System.out.println("Sprzedający nie ma telefonu");
+            } else if (buyer.cash < price) {
+                System.out.println("Za mało hajsu!");
+            } else {
+                seller.cash += price;
+                buyer.cash -= price;
+                seller.mobile = null;
+                buyer.mobile = this;
+                System.out.println("Telefon "+ this.producer + " sprzedany za "+ price);
+            }
+        }
+        @Override
+        public String toString() {
+            return "Phone{" + "yearOfPRoduction=" + yearOfPRoduction + ", producer='" + producer + '\'' +  ", model='" + model + '\'' +
+                    ", screenSize=" + screenSize +
+                    ", os='" + os + '\'' +
+                    '}';
+        }
+
+        @Override
+        public void sale() {
+
+        }
+
+        @Override
+        public void getPrice() {
+
+        }
+
+
+
+        public void installAnApp(String nazwaAplikacji) {
+            installAnApp(nazwaAplikacji, DEFAULT_VERSION);
+        }
+
+        public void installAnApp(String nazwaAplikacji, String wersjaAplikacji) {
+            installAnApp(nazwaAplikacji, wersjaAplikacji, DEFAULT_SERVER_ADDRESS);
+        }
+
+        public void installAnApp(String nazwaAplikacji, String wersjaAplikacji, String adresServera) {
+            System.out.println("Zainstalowano aplikacje " + nazwaAplikacji + " wersja:  " + wersjaAplikacji +
+                    " server:  " + adresServera);
+        }
+
+        public void installAnApp(List<String> applicationsToInstall) {
+            for (String application : applicationsToInstall) {
+                installAnApp(application);
+            }
+        }
+
+        public void installAnApp(URL url) {
+            System.out.println("url: " + url);
+            System.out.println("Aplikacja: " + DEFAULT_APPLICATION + " Wersja " + DEFAULT_VERSION);
+        }
+
+
     }
-}
