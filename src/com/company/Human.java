1695d7dde16cb5ce375d2b29c94652a22629979e
Mikolaj <niklaus7@me.com>
2022-01-21 18:34:48 +0100

    update

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,66 +3,169 @@ package com.company;
 import com.company.Devices.Car;
 import com.company.Devices.Phone;
 
-public class Human extends Animal {
+import java.util.Arrays;
 
-    public final static String HUMAN_SPECIES = "homo sapiens";
+public class Human extends Animal {
+    public final static String HUMAN_SPECIES = "Homo sapiens";
+    private static final int DEFAULT_GARAGE_SIZE = 4;
+    private static final Double DEFAULT_SALARY = 1600.0;
     public Double cash;
+    public Car car;
+    public Human animal;
     String firstName;
     String lastName;
-    Animal animal;
-    public Car car;
-    Double salary;
+    public Animal pet;
     public Phone mobile;
+    public Car[] garage;
+    private Double salary;
+    private boolean device;
 
-
-    Human(String firstName) {
+    Human(String firstName, String lastName) {
         super(HUMAN_SPECIES);
         this.firstName = firstName;
-        this.cash = 0.0;
+        this.lastName = lastName;
+        this.salary = DEFAULT_SALARY;
+        this.cash=-0.0;
+        this.garage = new Car[DEFAULT_GARAGE_SIZE];
+    }
+    Human(String firstName,String lastName, int garageSize) {
+        super(HUMAN_SPECIES);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = DEFAULT_SALARY;
+        this.cash=-0.0;
+        this.garage = new Car[garageSize];
     }
 
-    public void getSalary() {
-        System.out.println();
+    public Human(String ala) {
+        super();
     }
 
+    public void sale(Human seller, Human buyer, double price) {
+        System.out.println("NIE");
+    }
+
+
+    public void setSalary(Double newSalary) {
+        if (newSalary >= 0) {
+            salary = newSalary;
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego");
+            System.out.println("Należy odebrać aneks do umowy od pani Hani z kadr");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty, nie ma co ukrywać dochodu :)");
+        }
+        else
+        {
+            System.out.println("Ujemna wyplata!!");
+        }
+    }
+    public void setCar(Car vehicle)
     {
-    }
-
-    public void setSalary(Double salary) {
-        if(salary < 0) {
-            System.out.println("Salary nie moze byc mniejsze niz 0");
-        }else {
-            System.out.println("Dane zostaly wyslane do systemu ksiegowego");
-            System.out.println("Koniecznie odbierz aneks do umowy u pani Hani z kadr");
-            System.out.println("ZUS i US juz wiedza o zmianie wyplaty i nie ma sensu ukrywac dochodu");
-            this.salary = salary;
+        Object device;
+        if(salary > vehicle.value)
+        {
+            System.out.println("Udało się kupić samochód za gotówkę!");
+            device = vehicle.showCar();
+        }
+        else if(salary > (vehicle.value/12))
+        {
+            System.out.println("Udało się kupić samochód na kredyt!");
+            device = vehicle.showCar();
+        }
+        else
+        {
+            System.out.println("Zapisz się na studia i znajdź nową robotę albo idź po podwyżkę!");
         }
     }
 
-    public Car getCar() {
-        return car;
+    public Double getSalary(){
+
+        System.out.println("Ostatnie pobieranie danych o wypłacie: 10.09.2021. Wartość: "+ salary+ "zl");
+        return this.salary;
+    }
+    public void getCar()
+    {
+        System.out.println(this.device);
+    }
+    //toString
+    public Car getCar(Integer carPosition)
+    {
+        return this.garage[carPosition];
+    }
+    public double calculateSumOfCarsValue() {
+        double sumOfValue = 0;
+        for (Car car : garage
+        ) {
+            if (car != null)
+                sumOfValue += car.value;
+        }
+        return sumOfValue;
+    }
+    public Phone getPhone() {
+        return mobile;
     }
 
-    public void checkIfHumanCanBuyCar(Car car){
-        if (salary > car.getValue()){
-            System.out.println("Udalo sie kupic samochod za gotowke");
-            this.car = car;
-        }else if(salary/12> car.getValue()) {
-            System.out.println("Udalo sie kupic samochod na kredyt");
-            this.car = car;
-        }else {
-            System.out.println("zapisz sie na studia i znajdz nowa robote albo idz po podwyzke");
-        }
+    public void setPhone(Phone phone) {
+        this.mobile = phone;
+        phone.phoneOwner = this;
+    }
+    //toString
+    public String toString()
+    {
+        return "Imię: " + firstName + " Nazwisko: " + lastName +
+                "\nZwierzę: (" + pet + ") Samochód:( " + Arrays.asList(garage) + ") " +
+                "\nPensja: " + salary + "Telefon: "+ mobile;
     }
 
     @Override
-    public String toString() {
-        return "Human{" +
-                "animal=" + animal +
-                ", car=" + car +
-                ", salary=" + salary +
-                '}';
+    public void feed(double foodWeight, String foodType) {
+
+    }
+
+    public void sell(Human seller,Human buyer,Double price){
+        System.out.println("Nie mozxna sprzedawac ludzi !!!");
+    }
+    public boolean hasCar(Car newCar){
+        for(Car car : garage){
+            if(car != null && car.equals(newCar)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean hasFreeParkingLot(){
+        for(int i = 0;i<this.garage.length;i++){
+            if(garage[i]== null){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void removeCar(Car carToRemove) {
+        for (int i = 0 ; i< garage.length ; i++) {
+            if(garage[i].equals(carToRemove)){
+                garage[i] = null;
+                return;
+            }
+        }
+    }
+    public boolean addCar(Car car){
+        for (int i = 0; i <garage.length; i++) {
+            if (garage[i]  == null) {
+                garage[i] = car;
+                return true;
+            }
+        }
+        return false;
     }
 
 
+    @Override
+    public void sale() {
+
     }
+
+    @Override
+    public void getPrice() {
+
+    }
+}
